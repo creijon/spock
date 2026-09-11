3ea1e594dab3cc4372a281f1c577e1ae18bd1aa2
Jon Creighton <jon.creighton@gmail.com>
2026-09-11 06:10:37 +0200

    Revert the file watcher in Splat

--- src/samples/splat.cpp
+++ src/samples/splat.cpp
@@ -207,30 +207,34 @@ public:
     void createGraphicsPipeline(vk::ShaderStageFlags shaderStages = vk::ShaderStageFlagBits::eAllGraphics)
     {
         glslang::InitializeProcess();
+        vk::raii::ShaderModule vertexShader{ nullptr };
+        vk::raii::ShaderModule fragmentShader{ nullptr };
         try
         {
             if (shaderStages & vk::ShaderStageFlagBits::eVertex)
             {
-                m_vertexShader = spock::loadShader(m_device, vk::ShaderStageFlagBits::eVertex, SHADER_PATH + VERTEX_SHADER);
+                vertexShader = spock::loadShader(m_device, vk::ShaderStageFlagBits::eVertex, SHADER_PATH + VERTEX_SHADER);
             }
 
             if (shaderStages & vk::ShaderStageFlagBits::eFragment)
             {
-                m_fragmentShader = spock::loadShader(m_device, vk::ShaderStageFlagBits::eFragment, SHADER_PATH + FRAGMENT_SHADER);
+                fragmentShader = spock::loadShader(m_device, vk::ShaderStageFlagBits::eFragment, SHADER_PATH + FRAGMENT_SHADER);
             }
         }
         catch (std::exception const& e)
         {
             spock::writeLog(std::string(e.what()));
+            glslang::FinalizeProcess();
+            throw;
         }
         glslang::FinalizeProcess();
 
-        if (m_vertexShader != nullptr && m_fragmentShader != nullptr)
+        if (vertexShader != nullptr && fragmentShader != nullptr)
         {
             const vk::PipelineShaderStageCreateFlags shaderStageCreateFlags{};
             std::vector<vk::PipelineShaderStageCreateInfo> shaderStagesInfo{
-                {shaderStageCreateFlags, vk::ShaderStageFlagBits::eVertex, *m_vertexShader, "main"},
-                {shaderStageCreateFlags, vk::ShaderStageFlagBits::eFragment, *m_fragmentShader, "main"},
+                {shaderStageCreateFlags, vk::ShaderStageFlagBits::eVertex, *vertexShader, "main"},
+                {shaderStageCreateFlags, vk::ShaderStageFlagBits::eFragment, *fragmentShader, "main"},
             };
 
             spock::VertexFormat vertexFormat;
@@ -273,8 +277,6 @@ private:
     vk::raii::DescriptorSetLayout m_descriptorSetLayout{nullptr};
     vk::raii::PipelineLayout m_pipelineLayout{nullptr};
     vk::raii::Pipeline m_graphicsPipeline{nullptr};
-    vk::raii::ShaderModule m_vertexShader{nullptr};
-    vk::raii::ShaderModule m_fragmentShader{nullptr};
 
     // Dynamic data
     struct PerFrameData
@@ -347,48 +349,11 @@ protected:
             cameraMoved = true;
         }
 
-        m_watcher.notifyRenderer(renderer);
-
         renderer->update(m_scene, m_camera, cameraMoved, m_window.extents());
         m_previousCursor = cursor;
     }
 
 private:
-    class Watcher : public spock::FileWatcher
-    {
-    public:
-        Watcher() : spock::FileWatcher(SHADER_PATH)
-        {}
-
-        void fileModified(std::string const& filename) override
-        {
-            std::unique_lock lock(mutex);
-            if (filename == VERTEX_SHADER) modifiedShaders |= vk::ShaderStageFlagBits::eVertex;
-            if (filename == FRAGMENT_SHADER) modifiedShaders |= vk::ShaderStageFlagBits::eFragment;
-        }
-
-        void notifyRenderer(SplatRenderer* renderer)
-        {
-            vk::ShaderStageFlags temp;
-            {
-                // Take the lock for the minimum amount of time to avoid blocking the file watcher thread.
-                std::unique_lock lock(mutex);
-                temp = modifiedShaders;
-                modifiedShaders = vk::ShaderStageFlags(0);
-            }
-            if (temp)
-            {
-                // If the shader source is changed then rebuild the shaders and recreate the graphics pipeline.
-                renderer->waitIdle();
-                renderer->createGraphicsPipeline(temp);
-            }
-        }
-
-    private:
-        vk::ShaderStageFlags modifiedShaders{ vk::ShaderStageFlagBits::eAllGraphics };
-        std::mutex mutex;
-    };
-
     void loadScene(const std::string& filename)
     {
         try
@@ -408,8 +373,6 @@ private:
 
     vk::Offset2D m_previousCursor{};
     spock::OrbitCamera m_camera{glm::vec3(0.0f), 5.0f, 5.0f};
-
-    Watcher m_watcher;
 };
 
 int main()
